1bc6201fe897b4724689d4e8543c126df50f7e0e
Administrator <Administrator@koitt03a-PC>
2019-11-11 17:43:54 +0900

    2019-11-11

---
+++ src/com/ajax/dao/BoardDAO.java
@@ -0,0 +1,12 @@
+package com.ajax.dao;
+
+import java.util.List;
+import java.util.Map;
+
+public interface BoardDAO {
+	Map<String,String> selectBoard(Map<String,String> board);
+	List<Map<String,String>> selectBoardList(Map<String,String> board);
+	int insertBoard(Map<String,String> board);
+	int updateBoard(Map<String,String> board);
+	int deleteBoard(Map<String,String> board);
+}
---
+++ src/com/ajax/service/impl/BoardServiceImpl.java
@@ -0,0 +1,42 @@
+package com.ajax.service.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import com.ajax.dao.BoardDAO;
+import com.ajax.dao.impl.BoardDAOImpl;
+import com.ajax.service.BoardService;
+
+public class BoardServiceImpl implements BoardService {
+
+	@Override
+	public Map<String, String> selectBoard(Map<String, String> board) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Map<String, String>> selectBoardList(Map<String, String> board) {
+		BoardDAO bdao = new BoardDAOImpl();
+		return bdao.selectBoardList(board);
+	}
+
+	@Override
+	public Map<String, String> insertBoard(Map<String, String> board) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, String> updateBoard(Map<String, String> board) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, String> deleteBoard(Map<String, String> board) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
